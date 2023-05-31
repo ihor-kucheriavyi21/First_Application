83f5e54a8651eb69f67f19b978430024d878702c
ihor-kucheriavyi21 <kucherjavyj228@gmail.com>
2023-05-31 14:24:33 +0300

    new layout and button handler

--- app/src/main/java/com/devexpert/firstappliacation/MainActivity.java
+++ app/src/main/java/com/devexpert/firstappliacation/MainActivity.java
@@ -2,15 +2,22 @@ package com.devexpert.firstappliacation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.new_layout);
+        Button clickMeButton = findViewById(R.id.buttonId1);
+        TextView textView = findViewById(R.id.textView1);
+        clickMeButton.setOnClickListener(listener->{
+            textView.setText("Button clicked!!");
+            textView.setTextColor(Color.RED);
+        });
     }
 }
